bd1f954727553f7158898f0e4cacbabc752304ac
Luis Cordero <LFCordero@student.fullsail.edu>
2022-05-12 15:24:21 -0400

    Made Luis_works off of the old "Luis" branch, the old branch was giving me issues, so I started fresh on a new one

--- app/src/main/java/com/firstapp/tempus/MainActivity.kt
+++ app/src/main/java/com/firstapp/tempus/MainActivity.kt
@@ -2,10 +2,36 @@ package com.firstapp.tempus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        //Initializes the textView to display the current date
+
+        val sdf = SimpleDateFormat("M/dd/yyyy")
+        val currentDate = sdf.format(Date())
+        textView.text = currentDate
+
+
+
+        //a listener that checks when the date changes, allows the text box to show the selected day
+        calendarView.setOnDateChangeListener{view,year,month,dayOfMonth ->
+            val newMonth = month + 1
+            val calText = "$newMonth/$dayOfMonth/$year"
+            textView.text = calText
+
+
+        }
+
+
     }
 }

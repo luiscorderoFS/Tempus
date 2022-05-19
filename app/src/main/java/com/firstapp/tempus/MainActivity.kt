b9f53077a0cbc6ba3dc418aa69d742f66c99f93c
luiscorderoFS <94846739+luiscorderoFS@users.noreply.github.com>
2022-05-19 18:16:45 -0400

    Added recycler view

--- app/src/main/java/com/firstapp/tempus/MainActivity.kt
+++ app/src/main/java/com/firstapp/tempus/MainActivity.kt
@@ -12,26 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val textView = findViewById<TextView>(R.id.textView)
-
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
-        //Initializes the textView to display the current date
 
-        val sdf = SimpleDateFormat("M/dd/yyyy")
+        //region Text view for date change
+
+        //Initializes the textView to display the current date
+        val sdf = SimpleDateFormat("M/dd/yyyy", Locale.US)
         val currentDate = sdf.format(Date())
         textView.text = currentDate
-
-
 
         //a listener that checks when the date changes, allows the text box to show the selected day
         calendarView.setOnDateChangeListener{view,year,month,dayOfMonth ->
             val newMonth = month + 1
             val calText = "$newMonth/$dayOfMonth/$year"
             textView.text = calText
-
-
         }
-
+        //endregion
 
     }
 }

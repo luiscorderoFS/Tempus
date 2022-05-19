d2c397c90f838141b1697e2b3bd4a623601ccccd
Thomas Nguyen <TMNguyen2@student.fullsail.edu>
2022-05-19 18:54:10 -0400

    Added CreateEventActivity

---
+++ app/src/main/java/com/firstapp/tempus/CreateEventActivity.kt
@@ -0,0 +1,11 @@
+package com.firstapp.tempus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
